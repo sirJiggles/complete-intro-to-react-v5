3327aa952a0bd74c243edaf8ec2005e1aee6a1fb
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-05-06 08:26:01 +0200

    messing arround with routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { Router } from "@reach/router";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-const Details = () => {
-  return <h1>hi!</h1>;
+const Details = props => {
+  return <pre>{JSON.stringify(props, null, 4)}</pre>;
+  // return <h1>hi!</h1>;
 };
 
 export default Details;
