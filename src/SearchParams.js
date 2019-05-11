369cadac5996ac9e2ddf6f88f2d3bd5108c5b8a7
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2019-05-11 17:47:37 +0200

    update the global state

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -10,7 +10,7 @@ const SearchParams = () => {
   const [pets, setPets] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
-  const [theme] = useContext(ThemeContext);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   async function requestPets() {
     const { animals } = await pet.animals({
@@ -53,6 +53,21 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
+        {/* DO not use dropdown as dropdown makes it's own hooks and we are
+        using the apps hooks here */}
+        <label htmlFor="theme">
+          Theme:
+          <select
+            value={theme}
+            onChange={e => setTheme(e.target.value)}
+            onBlur={e => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Darkblue</option>
+            <option value="hotpink">Hotpink</option>
+            <option value="green">Green</option>
+          </select>
+        </label>
         <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
       <Results pets={pets} />
